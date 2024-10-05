810664e19cfe7cd11f7aeaa8af75cd94f1dbccfc
Caheri Salas <csalas.navarrete@gmail.com>
2024-10-05 03:19:17 -0600

    Corrige error en la función de cálculo de pronóstico

---
+++ hello_world.py
@@ -0,0 +1,7 @@
+print("En una noche obscura es mejor estar borracho,
+	En una noche obscura no puedes ver más allá de tu nariz, 
+	pero si estás borracho no importa que no veas,
+	porque puedes sentir el dolor de toda la ciudad, 
+	puedes sentir la pena de cada ser vivo
+	y puedes llorar con ellos.")
+
