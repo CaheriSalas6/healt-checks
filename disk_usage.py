439a48afffb7aaf370a2e74f22b9f3d93711ff7c
Cahei Nachito Salas <csalas.navarrete@gmail.com>
2024-10-25 12:16:09 -0600

    modified:   disk_usage.py

--- disk_usage.py
+++ disk_usage.py
@@ -4,23 +4,20 @@ import shutil
 import sys
 
 def check_disk_usage(disk, min_absolute, min_percent):
-	
-	"""Returns True if there is enough free disk space, false otherwise."""
-	
-	du = shutil.disk_usage(disk)
-	
-	#Calculate de percentaje of free space
-	percent_free = 100 * du.free / du.total
+    """Returns True if there is enough free disk space, false otherwise."""
+    du = shutil.disk_usage(disk)
+    # Calculate the percentage of free space
+    percent_free = 100 * du.free / du.total
+    # Calculate how many free gigabytes
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_absolute:
+        return False
+    return True
 
-	#Calculate how many free gigabytes
- 	gigabytes = du.free /2**30
-	if percent_free < min_percent or gigabytes_free < min_absolute:
-		return False
-	return True
-
-#Check for at least 2 GB and 10 % is free 
+# Check for at least 2 GB and 10% free
 if not check_disk_usage("/", 2, 10):
-	print("ERROR: Not enough disk space")
-	sys.exit(1)
-print("Everetything ok.")
-sys.exit(0) 
+    print("ERROR: Not enough disk space")
+    sys.exit(1)
+
+print("Everything ok")
+sys.exit(0)
